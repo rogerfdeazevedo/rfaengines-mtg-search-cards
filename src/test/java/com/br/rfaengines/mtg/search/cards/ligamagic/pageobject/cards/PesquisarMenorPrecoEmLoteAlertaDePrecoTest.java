ccb2190078cf06971935c80a6712624e9b469ae4
Roger Fortunato de Azevedo <roger_azevedo@terceiros.sicredi.com.br>
2018-11-28 23:23:10 -0200

    code review

--- src/test/java/com/br/rfaengines/mtg/search/cards/ligamagic/pageobject/cards/PesquisarMenorPrecoEmLoteAlertaDePrecoTest.java
+++ src/test/java/com/br/rfaengines/mtg/search/cards/ligamagic/pageobject/cards/PesquisarMenorPrecoEmLoteAlertaDePrecoTest.java
@@ -19,84 +19,99 @@ public class PesquisarMenorPrecoEmLoteAlertaDePrecoTest extends BaseTest {
 	@Test(groups = { "Ligamagic" }, description = "Pesquisar o menor preco de cada carta a partir de uma lista.")
 	public void pesquisarMenorPreco() {
 
-		StringBuilder dirArquivo = new StringBuilder();
-		dirArquivo.append(PropertiesUtil.getValue("lista.dir")).append(PropertiesUtil.getValue("lista.arquivo"));
+		ArrayList<String> listaDeArquivos;
+		ArrayList<String> listaDeCartas;
+		ArrayList<String> listaDeCartasParaExportar;
+		String arquivoProcessado;
 
-		ArrayList<String> listaDeCartasImportada = ArquivosUtil.lerArquivo(dirArquivo.toString());
-		
+		String dirArquivos = PropertiesUtil.getValue("files.dir.import");
 
-		assertTrue(!listaDeCartasImportada.isEmpty(),
-				"Falha ao ler arquivo - Verifique se o arquivo existe: " + dirArquivo.toString());
-		
-		ArrayList<String> listaDeCartasParaExportar = new ArrayList<String>();	
-		
-		listaDeCartasParaExportar.add("qtd; nome-PT-BR; nome-ENG-US; cor; custo-de-mana; curva-de-mana; tipo; preco-menor; comprar;");		
+		listaDeArquivos = ArquivosUtil.listarArquivosDeTexto(dirArquivos);
 
-		CardsPage pesquisa = new CardsPage();
+		assertTrue(!listaDeArquivos.isEmpty(), "Sem arquivos para processar no diretório: " + dirArquivos);
 
-		for (String itemDaLista : listaDeCartasImportada) {
+		for (String arquivo : listaDeArquivos) {
 			
-			StringBuilder cartaParaExportar = new StringBuilder(); 
-			
-			cartaParaExportar.append(itemDaLista.substring(0, 1)).append("; ");			
+			arquivoProcessado = dirArquivos + arquivo;
 
-			String nomeDaCarta = itemDaLista.substring(2);		
-			
+			listaDeCartas = ArquivosUtil.lerArquivo(arquivoProcessado);
 
-			if (!nomeDaCarta.isEmpty()) {
+			assertTrue(!listaDeCartas.isEmpty(), "Falha ao ler arquivo - Verifique se o arquivo existe: " + arquivo);
 
-				pesquisa.limparCampoNomeDoCard().preencherCampoNomeDoCard(nomeDaCarta).clicarNoBotaoDePesquisaLupa();
+			listaDeCartasParaExportar = new ArrayList<String>();
+			listaDeCartasParaExportar.add(
+					"qtd; nome-PT-BR; nome-ENG-US; cor; custo-de-mana; curva-de-mana; tipo; preco-menor; comprar;");
 
-				StringBuilder resultado = new StringBuilder();
+			CardsPage pesquisa = new CardsPage();
 
-				boolean achouCarta = pesquisa.resultadoDaPesquisa().pesquisaRealizadaComSucesso(nomeDaCarta);
+			for (String itemDaListaDeCartas : listaDeCartas) {
 
-				if (achouCarta) {
+				if (!itemDaListaDeCartas.isEmpty()) {
 
-					String nomePTBR = pesquisa.resultadoDaPesquisa().getNomeDoCardEmPTBR();
-					String nomeENG = pesquisa.resultadoDaPesquisa().getNomeDoCardEmENG();
-					String menorPreco = pesquisa.resultadoDaPesquisa().alertaDePreco().getMenorPreco();
-					
-					resultado.append("<b> PT-BR Nome: </b> ").append(nomePTBR)
-								.append("<br> <b> ENG-US Nome:  </b>").append(nomeENG)
-									.append("<br><b>Menor Preco: </b>").append(menorPreco).append("<br>");
+					StringBuilder cartaParaExportar = new StringBuilder();
 
-					ReportTestManager.adicionarEvidencia(resultado.toString());
-					
-					cartaParaExportar.append(nomePTBR).append("; ");
-					cartaParaExportar.append(nomeENG).append("; ");
-					cartaParaExportar.append(pesquisa.resultadoDaPesquisa().getCorDaCarta()).append("; ");
-					cartaParaExportar.append(pesquisa.resultadoDaPesquisa().getCustoDeMana()).append("; ");
-					cartaParaExportar.append(pesquisa.resultadoDaPesquisa().getCurvaDeMana()).append("; ");
-					cartaParaExportar.append(pesquisa.resultadoDaPesquisa().getTipoDaCarta()).append("; ");
-					cartaParaExportar.append(menorPreco).append("; ");
-					
-					listaDeCartasParaExportar.add(cartaParaExportar.toString());
+					cartaParaExportar.append(itemDaListaDeCartas.substring(0, 1)).append("; ");
 
-				} else {
+					String nomeDaCarta = itemDaListaDeCartas.substring(2);
 
-					resultado.append("<b> Nome Pesquisado: </b> ").append(nomeDaCarta)
-								.append("<br> Verifique se o nome da carta pesquisado esta correto.");
+					pesquisa.limparCampoNomeDoCard()
+								.preencherCampoNomeDoCard(nomeDaCarta)
+									.clicarNoBotaoDePesquisaLupa();
 
-					String dirEvidencias = ReportTestManager.getDirEvidencias();
-					String imagePath = "./evidencias/" + ScreenShot.takeScreenshot(dirEvidencias);
+					StringBuilder resultado = new StringBuilder();
 
-					ReportTestManager.getTest().log(LogStatus.WARNING,
-							resultado.toString() + "<br>" + ReportTestManager.getTest().addScreenCapture(imagePath));
+					boolean achouCarta = pesquisa.resultadoDaPesquisa().pesquisaRealizadaComSucesso(nomeDaCarta);
+
+					if (achouCarta) {
+
+						String nomePTBR = pesquisa.resultadoDaPesquisa().getNomeDoCardEmPTBR();
+						String nomeENGUS = pesquisa.resultadoDaPesquisa().getNomeDoCardEmENG();
+						String menorPreco = pesquisa.resultadoDaPesquisa().alertaDePreco().getMenorPreco();
+
+						resultado.append("<b> PT-BR Nome: </b> ").append(nomePTBR)
+									.append("<br> <b> ENG-US Nome:  </b>").append(nomeENGUS)
+										.append("<br><b>Menor Preco: </b>").append(menorPreco).append("<br>");
+
+						ReportTestManager.adicionarEvidencia(resultado.toString());
+
+						cartaParaExportar.append(nomePTBR).append("; ");
+						cartaParaExportar.append(nomeENGUS).append("; ");
+						cartaParaExportar.append(pesquisa.resultadoDaPesquisa().getCorDaCarta()).append("; ");
+						cartaParaExportar.append(pesquisa.resultadoDaPesquisa().getCustoDeMana()).append("; ");
+						cartaParaExportar.append(pesquisa.resultadoDaPesquisa().getCurvaDeMana()).append("; ");
+						cartaParaExportar.append(pesquisa.resultadoDaPesquisa().getTipoDaCarta()).append("; ");
+						cartaParaExportar.append(menorPreco).append("; ");
+
+						listaDeCartasParaExportar.add(cartaParaExportar.toString());
+
+					} else {
+
+						resultado.append("<b> Nome Pesquisado: </b> ").append(nomeDaCarta)
+									.append("<br> Verifique se o nome da carta pesquisado esta correto. <br>");
+
+						String dirEvidencias = ReportTestManager.getDirEvidencias();
+						String imagePath = "./evidencias/" + ScreenShot.takeScreenshot(dirEvidencias);
+
+						ReportTestManager.getTest().log(LogStatus.WARNING, resultado.toString() + "<br>"
+								+ ReportTestManager.getTest().addScreenCapture(imagePath));
+
+					}
 
 				}
 
 			}
 
+			StringBuilder dirDestino = new StringBuilder();
+
+			dirDestino.append(ReportTestManager.getDirEvidencias().replace("evidencias", ""))
+						.append("resultado-da-pesquisa-ligamagic-[").append(arquivo.replace(".txt", "")).append("]-")
+							.append(DataHoraUtil.getData("yyyyMMddHHmmss")).append(".txt");
+
+			ArquivosUtil.salvarArquivo(dirDestino.toString(), listaDeCartasParaExportar);			
+
+			ArquivosUtil.renomearArquivoParaProcessado(arquivoProcessado);
+
 		}
-		
-		StringBuilder dirDestino = new StringBuilder();
-		
-		dirDestino.append(ReportTestManager.getDirEvidencias().replace("evidencias", ""))
-					.append("resultado-da-pesquisa-ligamagic-").append(PropertiesUtil.getValue("lista.arquivo")).append("-")
-					.append(DataHoraUtil.getData("yyyyMMddHHmmss")).append(".txt");
-		
-		ArquivosUtil.salvarArquivo(dirDestino.toString(), listaDeCartasParaExportar);
 
 	}
 
